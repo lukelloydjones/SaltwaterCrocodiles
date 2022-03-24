c1b20888dc95e59f883ee424f220dab873750524
Luke Lloyd-Jones <luke.lloydjones@uqconnect.edu.au>
2022-03-25 07:55:48 +1000

    Updated the find_kinships function to cleanly detect GGPs and FTPs

--- analysis/slim_simulation/rscripts/slim_process_funcs.R
+++ analysis/slim_simulation/rscripts/slim_process_funcs.R
@@ -2,8 +2,8 @@
 # Functions necessary to process SLiM output for looking at  
 # Derived from PLODf_2al_tablefree_kira.R
 # Authors: Kira Villiers
-# Date started: 14/01/2021
-# Date updated: 26/01/2021
+# Date started: 14/01/2022
+# Date updated: 23/03/2022
 # ==============================================================================
 
 library(SNPRelate)
@@ -68,13 +68,10 @@ SLiM_validate <- function(PLOD,
                      sampled = unique(c(k$longID1,k$longID2)))
   #ids$longid[match(kinferdata_g$info[,"Our_sample"],ids$indexid)]
   
-  k <- rbind(merge(k, k2, by.x=c("longID1","longID2"), by.y=c("ID1","ID2")),
-             merge(k, k2, by.x=c("longID1","longID2"), by.y=c("ID2","ID1")))
-  
   
   if (!is.null(pcrelate)) {
     # Work out the kinships, as estimated by PCRelate:
-    k3 <- k
+    k3 <- k2
     k3$status <- "U"
     k3$d1 <- k3$kin > 2^(-(1 + 3/2)) & k3$kin <= 2^(-(1 + 1/2)) #degree 1
     k3$d2 <- k3$kin > 2^(-(2 + 3/2)) & k3$kin <= 2^(-(2 + 1/2)) #degree 2
@@ -85,13 +82,13 @@ SLiM_validate <- function(PLOD,
     k3$status[k3$d2 == TRUE] <- "HSP" 
  
     
-    k <- rbind(merge(k,  k3[,c("ID1","ID2","status")], 
+    k2 <- rbind(merge(k2,  k3[,c("ID1","ID2","status")], 
                      by.x=c("ID1","ID2"), 
                      by.y=c("ID1","ID2")),
-               merge(k,  k3[,c("ID1","ID2","status")], 
+               merge(k2,  k3[,c("ID1","ID2","status")], 
                      by.x=c("ID1","ID2"), 
                      by.y=c("ID2","ID1")))
-    names(k)[names(k) == 'status'] <- 'estkinship.PC'
+    names(k2)[names(k2) == 'status'] <- 'estkinship.PC'
   }
   
   # attr(x = k, which="mean_UP")       <- attr(x = PLOD, which="mean_UP")
@@ -100,7 +97,7 @@ SLiM_validate <- function(PLOD,
   # attr(x = k, which="mean_POP")      <- attr(x = PLOD, which="mean_POP")
   
   #plotPLOD(k, validations=TRUE, ...)
-  return(k)
+  return(k2)
 }
 
 findkinships <- function(pedigree, k, sampled = NULL) {
@@ -136,6 +133,8 @@ findkinships <- function(pedigree, k, sampled = NULL) {
   colnames(ks) <- c("ID1","ID2","kinship")
   
   # Start the kinship hunting:
+  
+  print("Profiling parent-offspring pairs")
   # Find Parent Offspring Pairs in our sample
   POP <- data.table::rbindlist(list(pedigree[, .(id,mumid)], pedigree[, .(id,dadid)]), 
                                use.names=FALSE)
@@ -150,6 +149,8 @@ findkinships <- function(pedigree, k, sampled = NULL) {
   
   
   # Find all Full Sibling Pairs
+  
+  print("Profiling full-siblings")
   FSP <- data.table::data.table(NULL)
   # all parental pairs that occur multiple times in pedigree table
   pgroups <- unique(pedigree[duplicated(pedigree, by=c("mumid","dadid")),
@@ -169,6 +170,8 @@ findkinships <- function(pedigree, k, sampled = NULL) {
                           msg="Pedigree file seems wrong: found a pair that's 
                           apparently both parent/offspring and full siblings")
   nFSP <- sum(ks$kinship == "FSP", na.rm=TRUE)
+  
+  print("Profiling half-siblings")
   
   #Find half-sibling pairs. They have a parent and child in the sample but not both.
   HSP <- data.table(NULL)
@@ -191,25 +194,25 @@ findkinships <- function(pedigree, k, sampled = NULL) {
   # Profiling GGPs
   # --------------
   
-  
+  print("Profiling GGPs and HTPs")
   kks <- rbind(merge(k, ks, by.x = c("longID1", "longID2"), 
                             by.y = c("ID1", "ID2")),
                merge(k, ks, by.x = c("longID1","longID2"), 
                             by.y = c("ID2","ID1")))
-  table(k2$kinship)
+  #table(k2$kinship)
+  # 
+  # kks.sub <- kks[(kks$kin > 0.09  & kks$kin < 0.15) &
+  #               (kks$k0  > 0.35  & kks$k0 < 0.65)
+  #                & (kks$kinship != "HSP") , ]
   
-  k.sub <- k2[(k2$kin > 0.09  & k2$kin < 0.15) &
-              (k2$k0  > 0.35  & k2$k0 < 0.65)
-               & (k2$kinship != "HSP") , ]
+  #kks <- kks.sub
+  #max(abs(kks$age1 - kks$age2))
   
-  max(abs(kks$age1 - kks$age2))
-  
-  # id4538637 id4550946 i2230 i2682
-  for (i in seq(1, dim(ks)[1]))
+  for (i in seq(1, dim(kks)[1]))
   {
     if (i %% 200 == 0) {print(i)}
-    p1 <- pedigree[id == k$longID1[i], ]
-    p2 <- pedigree[id == k$longID2[i], ]
+    p1 <- pedigree[id == kks$longID1[i], ]
+    p2 <- pedigree[id == kks$longID2[i], ]
   
     p1.mum <- pedigree[id == p1$mumid, ]
     p1.dad <- pedigree[id == p1$dadid, ]
@@ -225,11 +228,11 @@ findkinships <- function(pedigree, k, sampled = NULL) {
     p2.gs <- c(p2.mum$mumid, p2.mum$dadid, 
                p2.dad$mumid, p2.dad$dadid)
     # Test - FTPs
-    if (sum(p1.gs %in% p2.ps) == 2 |
-        sum(p2.gs %in% p1.ps) == 2)
+    if ( (sum(p1.gs %in% p2.ps) == 2 & sum(p1.ps %in% p2$id) != 1) |
+         (sum(p2.gs %in% p1.ps) == 2 & sum(p2.ps %in% p1$id) != 1) )
     {
       print(i)
-      ks$kinship <- "FTP"
+      kks$kinship[i] <- "FTP"
       print('Aunt/Neice relationship')
     }
     
@@ -237,15 +240,14 @@ findkinships <- function(pedigree, k, sampled = NULL) {
     if (sum(p1.gs %in% p2$id) == 1 |
         sum(p2.gs %in% p1$id) == 1)
     {
-      print('i')
-      ks$kinship <- "GGP"
+      print(i)
+      kks$kinship[i] <- "GGP"
       print('GGP whaaat')
     }
   }
 
-  return(ks)
+  return(kks)
 }
-
 
 # Do preprocessing (MAF filtering + LD filtering) of crocodile dataset.
 #
@@ -256,6 +258,7 @@ findkinships <- function(pedigree, k, sampled = NULL) {
 # data is saved, the second a kinference dataframe, the third the PCRelate 
 # output dataframe, and the last a ggplot object containing the plot of
 # the first two PCs
+
 SLiM_preprocess <- function(homefolder, maxsample=0, nsnps=0, n.pcs=0,
                             genodata="nonWF_st1_gen_6000.vcf", pca=TRUE) {
   require(SNPRelate)
@@ -345,6 +348,7 @@ SLiM_preprocess <- function(homefolder, maxsample=0, nsnps=0, n.pcs=0,
 
 # Called by the _preprocess functions. Calls the PCRelate functions
 # from pcrelateModified_logistic.R
+
 runPCRelate <- function(homefolder, selectedind, selectedsnp, nsnps, n.pcs) {
   # Then run through PCRelate processing
   require(BiocParallel)
